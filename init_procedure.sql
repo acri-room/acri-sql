03d1ca9e4e2bf6daee341cc4e9116551a3dc670f
Naoki FUJIEDA <nf.longarch@gmail.com>
2023-04-20 17:14:42 +0900

    close regular maintainance slot (12:00- on 3rd Monday)

--- init_procedure.sql
+++ init_procedure.sql
@@ -52,12 +52,16 @@ begin
 	set @today = date_format(current_date(), '%Y-%m-01');
 	set @open_date = date_add(@today, interval 1 month);
 	set @close_date = date_add(@today, interval 2 month);
+	set @maintenance_day = date_sub(@open_date, interval 1 day);
+	set @maintenance_day = date_sub(@maintenance_day, interval weekday(@maintenance_day) day);
+	set @maintenance_day = date_add(@maintenance_day, interval 21 day);
+	set @maintenance_time = '12:00:00';
 
 	while @open_date < @close_date do
 		call open_dayall(@open_date, 3);
 		set @open_date = date_add(@open_date, interval 1 day);
 	end while;
-	call check_maintenance();
+	delete wp_olb_timetable from wp_olb_timetable where date = @maintenance_day and time = @maintenance_time;
 end;
 //
 
